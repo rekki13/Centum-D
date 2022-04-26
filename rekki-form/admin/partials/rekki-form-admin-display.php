1aca9dc5197f0fb3945599f31399ca09fc41b1ee
Danil <rrekkii@gmail.com>
2022-04-26 12:17:48 +0300

    Display user info in admin page from database

--- rekki-form/admin/partials/rekki-form-admin-display.php
+++ rekki-form/admin/partials/rekki-form-admin-display.php
@@ -13,9 +13,39 @@
  */
 ?>
 
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
-<div class="wrap">
-    <h1><?= __('ReKKi Form','rekki-form')?></h1>
+    <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+    <div class="wrap">
+        <h1><?= __( 'ReKKi Form', 'rekki-form' ) ?></h1>
 
-    Shortcode: <code>[shortcode-rekki-form]</code>
-</div>
+        Shortcode: <code>[shortcode-rekki-form]</code>
+    </div>
+
+<?php
+global $wpdb;
+// this adds the prefix which is set by the user upon instillation of wordpress
+$table_name = $wpdb->prefix . "rekki_form";
+// this will get the data from your table
+$retrieve_data = $wpdb->get_results( "SELECT * FROM $table_name ORDER BY rekki_form_phone  ASC" );
+?>
+    <table id="rekki_users_request">
+        <tr>
+            <td><h2>Full Name</h2></td>
+            <td><h2>Email</h2></td>
+            <td><h2>Phone</h2></td>
+            <td><h2>Date</h2></td>
+        </tr>
+		<?php
+		foreach ( $retrieve_data as $retrieved_data ) { ?>
+            <tr>
+                <td><?= $retrieved_data->rekki_form_full_name; ?></td>
+                <td>
+                    <a href="mailto:<?= $retrieved_data->rekki_form_email; ?>"><?= $retrieved_data->rekki_form_email; ?></a>
+                </td>
+                <td><a href="tel:<?= $retrieved_data->rekki_form_phone; ?>"><?= $retrieved_data->rekki_form_phone; ?></a></td>
+                <td><?= $retrieved_data->rekki_form_date; ?></td>
+            </tr>
+			<?php
+		}
+		?>
+    </table>
+<?php
